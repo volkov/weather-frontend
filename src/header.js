f2b0d4f145d314f23b084747fd7755d4f7102323
Sergey A Volkov <serg-v@yandex-team.ru>
2020-08-21 17:27:39 +0300

    header fixes

--- src/header.js
+++ src/header.js
@@ -20,7 +20,7 @@ const Header = props => {
             align="center"
             justify="space-between"
             wrap="wrap"
-            padding="1.5rem"
+            padding="1rem"
             bg="teal.500"
             color="white"
             {...props}
@@ -30,18 +30,6 @@ const Header = props => {
                     Weather
                 </Heading>
             </Flex>
-
-            <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
-                <svg
-                    fill="white"
-                    width="12px"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <title>Menu</title>
-                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                </svg>
-            </Box>
 
             <Box
                 display={{ sm: show ? "block" : "none", md: "flex" }}
